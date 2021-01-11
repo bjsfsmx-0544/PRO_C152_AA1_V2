53a588c50be38a4ca331315c9b3d498ad846c311
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-01-11 18:45:01 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -8,26 +8,4 @@ AFRAME.registerComponent('log', {
       }
   });
 
-  AFRAME.registerComponent('name', {
-    schema: {
-      //data
-    },
-  
-    init: function () {
-      // Do something when component first attached.
-    },
-  
-    update: function () {
-      // Do something when component's data is updated.
-    },
-  
-    remove: function () {
-      // Do something the component or its entity is detached.
-    },
-  
-    tick: function () {
-      // Do something on every scene tick or frame.
-    }
-  });
-
-
+ 
