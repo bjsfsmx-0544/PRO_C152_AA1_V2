0eb4daf715e6dc6b3c61a0283112d7f4e393c0dc
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-01-11 18:45:46 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -6,5 +6,12 @@ AFRAME.registerComponent("move-box", {
 
   tick: function () {
 
+    this.data.moveX = this.data.moveX + 0.01;
+
+    var pos = this.el.getAttribute("position");
+
+    pos.x = this.data.moveX;
+
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
   }
 });
